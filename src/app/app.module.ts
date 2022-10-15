cd86a45b42d6a8536ce1f187174554479bf86440
LHPL\ADMIN <lehoangphuclam@gmail.com>
2022-10-15 19:13:39 +0700

    get hero by id, catchError

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './app-messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,14 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,20 +6,63 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import {catchError, map, tap } from 'rxjs';
+
+
+
+
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
 
-  constructor(private messageService : MessageService) { }
+  constructor(
+    private httpClient : HttpClient,
+    private messageService : MessageService
+  ) 
+    { 
 
+    }
+
+  private heroesUrl = 'api/heroes';
+    
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes.')
+    //this.messageService.add('HeroService: fetched heroes.');
+    //this.log('fetched heroes');
+    //const heroes = of(HEROES);
+
+    // RxJS tap() operator enable ability by looking at the observable values and send the message, using log() method
+    const heroes = this.httpClient.get<Hero[]>(this.heroesUrl)
+              .pipe(
+                  tap(_ => this.log('fetched heroes')),
+                  catchError(this.handleError('getHeroes', []))
+              );
+      
+
     return heroes;
   }
 
-  getHero(id: number): Observable<Hero>{
+  /**
+   * 
+   * @param operation - nameof operation that failed
+   * @param result - optional value to return as the observable result
+   * @returns 
+   */
+  private handleError<T>(operation = 'operation', result?: T){
+    return (error: any) : Observable<T> =>{
+      
+        console.log(error);
+        this.log(`${operation} failed: ${error.message}`);
+
+        // let the app keep running by returning an empty result
+
+        return of(result as T);
+    };
+  }
+
+  getHero_old(id: number): Observable<Hero>{
 
     // for now, assume the hero with the spcified id always exists.
     // error handling will be handle in the next section
@@ -28,4 +71,22 @@ export class HeroService {
     return of(hero);
 
   }
+
+  /** get a hero by id. Will 404 if id not found */
+  getHero(id: number) : Observable<Hero>{
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.httpClient.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+
+    );
+
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
 }
